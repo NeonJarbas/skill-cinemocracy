ab806a7f4807fcd047d0cec04ebe789064d04e97
JarbasAi <jarbasai@mailfence.com>
2024-01-07 04:36:47 +0000

    refactor/ocp

---
+++ README.md
@@ -0,0 +1,31 @@
+# <img src='./ui/cinemocracy.png' card_color='#40DBB0' width='50' height='50' style='vertical-align:bottom'/>Cinemocracy Collection
+
+## About
+
+In the early 1940s, the United States government commissioned some of the best filmmakers to create propaganda in support of the war effort. The works of the most famous of those directors, John Ford, John Huston, Frank Capra, and William Wyler, are the subjects of this collection. In contrast to today's environment (where many politicians think of Hollywood as the devil and most contemporary political cinema targets the government as irrational and inequitable) in the 1940s, everyone appeared to be on the same side. The films in this collection are examples of Cinemocracy, the relationship between motion pictures and government. These films are from the personal collection of Eric Spiegelman.
+
+![](./gui.png)
+![](./gui2.png)
+
+
+## Examples
+
+* "open cinemocracy"
+* "play real war videos"
+* "play old war video"
+* "play cinemocracy"
+
+## Credits
+- JarbasAl
+- [AIIX](https://github.com/AIIX/) - GUI
+- [Cinemocracy](https://archive.org/details/cinemocracy?tab=about)
+
+## Category
+**Entertainment**
+**Information**
+
+## Tags
+#public-domain
+#war
+#video
+#entertainment
--- __init__.py
+++ __init__.py
@@ -1,72 +1,112 @@
-from ovos_workshop.skills.video_collection import VideoCollectionSkill
-from mycroft.skills.core import intent_file_handler
-from pyvod import Collection, Media
-from os.path import join, dirname, basename
-from ovos_workshop.frameworks.cps import CPSMatchType, CPSPlayback, \
-    CPSMatchConfidence
+import json
+from os.path import join, dirname
+
+from ovos_utils.ocp import MediaType, PlaybackType
+from ovos_workshop.decorators.ocp import ocp_search, ocp_featured_media
+from ovos_workshop.skills.common_play import OVOSCommonPlaybackSkill
 
 
-class CinemocracySkill(VideoCollectionSkill):
-
-    def __init__(self):
-        super().__init__("Cinemocracy")
-        self.supported_media = [CPSMatchType.GENERIC,
-                                CPSMatchType.VIDEO]
-        self.message_namespace = basename(dirname(__file__)) + ".jarbasskills"
+class CinemocracySkill(OVOSCommonPlaybackSkill):
+    def __init__(self, *args, **kwargs):
+        self.supported_media = [MediaType.DOCUMENTARY]
         # load video catalog
         path = join(dirname(__file__), "res", "cinemocracy.jsondb")
         logo = join(dirname(__file__), "res", "cinemocracy.png")
-        self.media_collection = Collection("cinemocracy", logo=logo, db_path=path)
         self.default_image = join(dirname(__file__), "ui", "cinemocracy.png")
         self.skill_logo = join(dirname(__file__), "ui", "cinemocracy.png")
         self.skill_icon = join(dirname(__file__), "ui", "cinemocracy.png")
         self.default_bg = join(dirname(__file__), "ui", "bg.jpeg")
-        self.media_type = CPSMatchType.VIDEO
-        self.playback_type = CPSPlayback.GUI
+        with open(path) as f:
+            self.archive = json.load(f)["cinemocracy"]
+        super().__init__(*args, **kwargs)
+        self.load_ocp_keywords()
 
-    # voice interaction
-    def get_intro_message(self):
-        self.speak_dialog("intro")
+    def load_ocp_keywords(self):
+        title = []
 
-    @intent_file_handler('home.intent')
-    def handle_homescreen_utterance(self, message):
-        self.handle_homescreen(message)
+        for data in self.archive:
+            t = data["title"].split("|")[0].split("(")[0].strip()
+            title.append(t)
+            if ":" in t:
+                t1, t2 = t.split(":", 1)
+                title.append(t1.strip())
+                title.append(t2.strip())
 
-    # better common play
-    def normalize_title(self, title):
-        title = title.lower().strip()
-        title = self.remove_voc(title, "cinemocracy")
-        title = self.remove_voc(title, "movie")
-        title = self.remove_voc(title, "play")
-        title = self.remove_voc(title, "video")
-        title = title.replace("|", "").replace('"', "") \
-            .replace(':', "").replace('”', "").replace('“', "") \
-            .strip()
-        return " ".join([w for w in title.split(" ") if w])  # remove extra spaces
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_name", title)
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_genre", ["war", "propaganda"])
+        self.register_ocp_keyword(MediaType.DOCUMENTARY,
+                                  "documentary_streaming_provider",
+                                  ["Cinemocracy",
+                                   "War Propaganda"])
 
-    def match_media_type(self, phrase, media_type):
-        score = 0
-        if self.voc_match(phrase, "video") or media_type == CPSMatchType.VIDEO:
-            score += 10
+    @ocp_search()
+    def search_db(self, phrase, media_type):
+        base_score = 15 if media_type == MediaType.DOCUMENTARY else 0
+        entities = self.ocp_voc_match(phrase)
 
-        if self.voc_match(phrase, "old"):
-            score += 10
+        title = entities.get("documentary_name")
+        skill = "documentary_streaming_provider" in entities  # skill matched
 
-        if self.voc_match(phrase, "real"):
-            score += 15
+        base_score += 30 * len(entities)
 
-        if self.voc_match(phrase, "war"):
-            score += 20
+        if title:
+            base_score += 30
+            candidates = [video for video in self.archive
+                          if title.lower() in video["title"].lower()]
+            for video in candidates:
+                yield {
+                    "title": video["title"],
+                    "match_confidence": min(100, base_score),
+                    "media_type": MediaType.DOCUMENTARY,
+                    "uri": video["streams"][0],
+                    "playback": PlaybackType.VIDEO,
+                    "skill_icon": self.skill_icon,
+                    "skill_id": self.skill_id
+                }
 
-        if self.voc_match(phrase, "propaganda"):
-            score += 20
+        if skill:
+            yield self.get_playlist()
 
-        if self.voc_match(phrase, "cinemocracy"):
-            score += 50
+    def get_playlist(self, score=50, num_entries=25):
+        pl = self.featured_media()[:num_entries]
+        return {
+            "match_confidence": score,
+            "media_type": MediaType.DOCUMENTARY,
+            "playlist": pl,
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "image": self.skill_icon,
+            "title": "Cinemocracy (Documentary Playlist)",
+            "author": "US Government"
+        }
 
-        return score
+    @ocp_featured_media()
+    def featured_media(self):
+        return [{
+            "title": video["title"],
+            "match_confidence": 70,
+            "media_type": MediaType.DOCUMENTARY,
+            "uri": video["streams"][0],
+            "playback": PlaybackType.VIDEO,
+            "skill_icon": self.skill_icon,
+            "skill_id": self.skill_id
+        } for video in self.archive]
 
 
-def create_skill():
-    return CinemocracySkill()
+if __name__ == "__main__":
+    from ovos_utils.messagebus import FakeBus
 
+    s = CinemocracySkill(bus=FakeBus(), skill_id="t.fake")
+    # "play war propaganda"
+    for r in s.search_db("play Why We Fight", MediaType.DOCUMENTARY):
+        print(r)
+        # {'title': 'Why We Fight: The Battle of Britain', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/BattleOfBritain/BattleOfBritain.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: The Battle of China', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/BattleOfChina/BattleOfChina.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: The Battle of Russia Part I', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/BattleOfRussiaI/BattleOfRussiaI.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: The Battle of Russia Part II', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/BattleOfRussiaII/BattleOfRussiaII.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: Divide and Conquer', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/DivideAndConquer/DivideAndConquer.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: Prelude to War', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/PreludeToWar/PreludeToWar.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: The Nazi Strike', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/TheNazisStrike/TheNazisStrike.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
+        # {'title': 'Why We Fight: War Comes to America', 'match_confidence': 75, 'media_type': <MediaType.DOCUMENTARY: 15>, 'uri': 'https://archive.org/download/WarComesToAmerica/WarComesToAmerica.mpeg', 'playback': <PlaybackType.VIDEO: 1>, 'skill_icon': 'https://github.com/OpenVoiceOS/ovos-ocp-audio-plugin/raw/master/ovos_plugin_common_play/ocp/res/ui/images/ocp.png', 'skill_id': 't.fake'}
